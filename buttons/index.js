da9f664cfa6afb30cd66f290229a9996e206dfec
Jakub Poznanski <jakubit.start@gmail.com>
2023-07-11 13:18:06 +0200

    adding a working subscribe button

--- buttons/index.js
+++ buttons/index.js
@@ -1,5 +1,15 @@
-const btn = document.getElementById("btn");
+var btn = document.getElementById("btn");
 
-btn.addEventListener('click', function onClick() {
-    
+btn.addEventListener("click", function onClick() {
+    document.getElementById("btn").className = "subscribed";
+    document.getElementById("btn").textContent = "Subskrybujesz";
+    document.getElementById("btn").id = "btn2";
+});
+
+var btn2 = document.getElementById("btn2");
+
+btn2.addEventListener("click", function onClick() {
+    document.getElementById("btn2").className = "subscribe";
+    document.getElementById("btn2").textContent = "Subskrybuj";
+    document.getElementById("btn").id = "btn";
 });
